3174235afffb711068e8ba1d1582f604730b7e83
Mykhailo Kan <michael.kan.mail@gmail.com>
2023-01-13 14:51:02 +0100

    add, multiply

--- eolympc/1008.cpp
+++ eolympc/1008.cpp
@@ -9,6 +9,8 @@ module by 2^8 (one byte) and you get decimal for first byte
 
 devide by 2^8 (one byte) and you get decimal for second byte
 
+столбик
+
 */
 
 #define MAX_CHARS 10000
@@ -16,41 +18,142 @@ devide by 2^8 (one byte) and you get decimal for second byte
 #define BYTE_SIZE 2000 // this is huge long t size
 #define HUGE_LONG_T_SIZE BYTE_SIZE
 #define BYTE_MAX 256
-
-typedef unsigned char byte;
+#define BYTE_T_MAX 256
+#define BYTE_T_MAX_VALUE 255
+typedef unsigned char byte_t;
 // has size of HUGE_LONG_T_SIZE
-typedef byte* huge_long_t; 
+// typedef byte* huge_long_t;
 
-char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-// converts char digit to decimal number
-int digitToDecimal(char digit)
+// least significant endian
+struct huge_long_t
 {
-    char *findChar;
-    int index;
+    size_t size;
+    byte_t *bytes;
+};
 
-    findChar = strchr(digits, digit);
-    index = (int)(findChar - digits);
-
-    return index;
+huge_long_t huge_long_t_make_zero(const size_t size)
+{
+    huge_long_t num;
+    num.bytes = (byte_t *)malloc(size * sizeof(byte_t));
+    num.size = size;
+    memset(num.bytes, 0, size);
+    return num;
 }
 
-// converts decimal num to digit
-// must be less than 36 or more or equal 0
-char decimalToDigit(int dec)
+huge_long_t huge_long_t_from_int(int integer) // int not const because we use this variable for calculation
 {
-    if (dec < 0 || dec > 36)
+    huge_long_t num = huge_long_t_make_zero(sizeof(int));
+    int ibytes = 0;
+    while (integer > 0)
+    {
+        // we put the value to byte
+        num.bytes[ibytes] = integer % BYTE_T_MAX;
+        // increment iBytenum to set it to empty byte in @bytenum
+        ibytes++;
+        // devide result
+        integer /= BYTE_T_MAX;
+    }
+    return num;
+}
+
+void huge_long_t_delete(const huge_long_t *const num)
+{
+    if (num->bytes)
+        free(num->bytes);
+}
+
+bool huge_long_t_is_zero(const huge_long_t num)
+{
+    for (size_t i = 0; i < num.size; i++)
+    {
+        if (num.bytes[i] != 0)
+            return false;
+    }
+    return true;
+}
+
+huge_long_t huge_long_t_copy(const huge_long_t num)
+{
+    huge_long_t num_copy = huge_long_t_make_zero(num.size);
+    for (size_t i = 0; i < num.size; i++)
+    {
+        num_copy.bytes[i] = num.bytes[i];
+    }
+    return num_copy;
+}
+
+void huge_long_t_add_assign(huge_long_t *const a, const huge_long_t b)
+{
+    if (a->size != b.size)
+    {
+        printf("a.size != b.size");
         exit(1);
-    return digits[dec];
+    }
+
+    int carry = 0;
+    for (size_t i = 0; i < a->size; i++)
+    {
+        // sum cannot be bigger than 255 + 255
+        int sum = a->bytes[i] + b.bytes[i] + carry;
+        if (sum > BYTE_T_MAX)
+        {
+            // max value we can put into this byte
+            a->bytes[i] = sum % BYTE_T_MAX;
+            // save to carry
+            carry = sum / BYTE_T_MAX;
+        }
+        else
+        {
+            a->bytes[i] = sum;
+            // flush carry because we have place to put values
+            carry = 0;
+        }
+    }
+}
+
+huge_long_t huge_long_t_multiply(const huge_long_t a, const huge_long_t b)
+{
+    if (a.size != b.size)
+    {
+        printf("a.size != b.size");
+        exit(1);
+    }
+    // we save multiplication into result var
+    huge_long_t result = huge_long_t_make_zero(a.size);
+    for (size_t i = 0; i < b.size; i++)
+    {
+        int carry = 0;
+        // inner multiplication (multiply one "b" byte on all "a" bytes )
+        for (size_t j = 0; j < a.size; j++)
+        {
+            // sum cannot be bigger than 255 * 255
+            int sum = a.bytes[j] * b.bytes[i] + carry;
+
+            if (sum > BYTE_T_MAX)
+            {
+                // max value we can put into this byte
+                result.bytes[j] += sum % BYTE_T_MAX;
+                // save to carry
+                carry = sum / BYTE_T_MAX;
+            }
+            else
+            {
+                result.bytes[j] += sum;
+                // flush carry because we have place to put values
+                carry = 0;
+            }
+        }
+    }
+    return result;
 }
 
 /*
 get last valid byte for @num
 @num is number that is from bytes
 */
-int getLastByte(byte *num)
+int getLastByte(const byte_t *const num, const size_t size)
 {
-    for (int i = BYTE_SIZE; i >= 0; --i)
+    for (int i = size; i >= 0; --i)
     {
         if (num[i] != 0)
         {
@@ -60,122 +163,7 @@ int getLastByte(byte *num)
     return 0;
 }
 
-// get num that is least significant endian
-byte *toBytes(char *num, int numericalSystem)
-{
-    // byte* bytenum = new byte[BYTE_SIZE];// cpp
-    byte *bytenum = (byte *)malloc(BYTE_SIZE * sizeof(byte)); // c
-    int iBytenum = 0;
-
-    int len = strlen(num);
-    ull result = 0;
-    ull numericalDigit = 1;
-    for (int i = len - 1; i >= 0; --i)
-    {
-        // convert num in numericalSystem to decimal
-        char digit = num[i];
-        int decimal = digitToDecimal(digit);
-
-        // multiply numerical digit on numerical system to get value in decimal
-        ull addToResult = (ull)(numericalDigit * (ull)decimal);
-        if (result + addToResult < 0)
-        {
-            while (result > BYTE_MAX || (i - 1 < 0 && result > 0))
-            {
-                // we put the value to byte
-                bytenum[iBytenum] = result % BYTE_MAX;
-                // increment iBytenum to set it to empty byte in @bytenum
-                iBytenum++;
-                // devide result
-                result /= BYTE_MAX;
-                // and reset numerical digit
-                numericalDigit = 1;
-            }
-        }
-        result += addToResult;
-        numericalDigit *= (ull)numericalSystem;
-
-        // if result is bigger than BYTE_MAX (2^8)
-        // dump all result 256 chunks into @bytenum
-        // OR
-        // if this is last iteration and result bigger than zero (then we have some values to save)
-        // BUG!!! if number is very big like 10000000000000000000000000000000
-        // with zeros this will completely broke, because result cannot hold that value
-        // we just hope that we will not encounter more consequent zeros than unsigned long long
-        while ((i - 1 < 0 && result > 0))
-        {
-            // we put the value to byte
-            bytenum[iBytenum] = result % BYTE_MAX;
-            // increment iBytenum to set it to empty byte in @bytenum
-            iBytenum++;
-            // devide result
-            result /= BYTE_MAX;
-            // and reset numerical digit
-            numericalDigit = 1;
-        }
-    }
-    return bytenum;
-}
-
-char *convertTo(byte *num, int numericalSystem)
-{
-    // not good, but good enough
-    // tmp is going to have converted number, but reversed
-    char tmp[1000];
-    int tmpLen = 0;
-    int size = getLastByte(num) + 1;
-    ull sumOfBytes = 0;
-    ull numericalDigit = 1;
-    for (int i = 0; i < size; i++)
-    {
-        ull addToSum = num[i] * numericalDigit;
-        // this is in case of overflow
-        if (sumOfBytes + addToSum < 0)
-        {
-            // if sumOfBytes bigger than numericalSystem
-            while (sumOfBytes > (ull)numericalSystem)
-            {
-                // calc remain, and convert remain to char digit
-                int remain = sumOfBytes % (ull)numericalSystem;
-                char digit = digits[remain];
-
-                // append char digit to the end of result string
-                tmp[tmpLen] = digit;
-                tmpLen++;
-
-                // divide sumOfBytes because we used for remains it
-                sumOfBytes /= (ull)numericalSystem;
-            }
-        }
-        sumOfBytes += addToSum;
-        numericalDigit *= 10;
-        // if it is last iteration and sumOfBytes bigger than zero (some stuff to save)
-        while ((i + 1 == size && sumOfBytes > 0))
-        {
-            // calc remain, and convert remain to char digit
-            int remain = sumOfBytes % (ull)numericalSystem;
-            char digit = digits[remain];
-
-            // append char digit to the end of result string
-            tmp[tmpLen] = digit;
-            tmpLen++;
-
-            // divide sumOfBytes because we used for remains it
-            sumOfBytes /= (ull)numericalSystem;
-        }
-    }
-
-    // reverse result string
-    char *result = (char *)malloc((size_t)(tmpLen + 1)); // + 1 for \0 character
-    for (int i = 0; i < tmpLen; i++)
-    {
-        result[i] = tmp[tmpLen - i - 1]; // - 1 because result len is not index it is size of @tmp
-    }
-    result[tmpLen] = '\0';
-    return result;
-}
-
-void printByte(byte b)
+void printByte(byte_t b)
 {
     for (int i = 0; i < 8; i++)
     {
@@ -183,47 +171,57 @@ void printByte(byte b)
     }
 }
 
-void printBytes(byte *num)
+void printBytes(const byte_t *const num, const size_t size, char sep = '\t')
 {
-    int size = getLastByte(num) + 1;
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
     {
         printByte(num[i]);
-        printf("\t");
+        if (sep != '\0')
+            printf("%c", sep);
     }
     printf("\n");
 }
 
-void printBytesReverse(byte *num)
+void printBytesReverse(const byte_t *const num, const size_t size, char sep = '\t')
 {
-    int size = getLastByte(num) + 1;
-    for (int i = size - 1; i >= 0; --i)
+    for (size_t i = 0; i < size; ++i)
     {
-        printByte(num[i]);
-        // printf("\t");
+        printByte(num[(size - 1) - i]);
+        if (sep != '\0')
+            printf("%c", sep);
     }
     printf("\n");
 }
 
 int main()
 {
-    int m = 0;
-    int k = 0;
-    // char n[MAX_CHARS_INPUT];
+    huge_long_t num = huge_long_t_from_int(123321);
+    huge_long_t num2 = huge_long_t_from_int(123321);
 
-    // scanf("%d %d", &m, &k);
-    // scanf("%s", n);
-    m = 10;
-    k = 10;
-    char n[] = "52282046404974150";
-    // convert input number to bytes representation
-    byte *num = toBytes(n, m);
-    printBytesReverse(num);
-    char *result = convertTo(num, k);
+    huge_long_t_add_assign(&num, num2);
 
-    printf("%s\n", result);
+    printf("%d\n", huge_long_t_is_zero(num));
+    printf("after addition num: \n");
+    printBytesReverse(num.bytes, num.size, ' ');
+    printf("num2: \n");
+    printBytesReverse(num2.bytes, num2.size, ' ');
+    // flush
+    huge_long_t_delete(&num);
+    huge_long_t_delete(&num2);
 
-    free(result);
-    free(num);
+    num = huge_long_t_from_int(321);
+    num2 = huge_long_t_from_int(123);
+
+    huge_long_t result = huge_long_t_multiply(num, num2);
+
+    printf("multiply result: \n");
+    printBytesReverse(result.bytes, result.size, ' ');
+    printf("num: \n");
+    printBytesReverse(num.bytes, num.size, ' ');
+    printf("num2: \n");
+    printBytesReverse(num2.bytes, num2.size, ' ');
+
+    huge_long_t_delete(&num);
+    huge_long_t_delete(&num2);
     return 0;
 }
